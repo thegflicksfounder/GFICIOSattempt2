b997ed79bde58ed948af96e7dcddab49b679b868
Afraz Siddiqui <afsiddiq@microsoft.com>
2021-03-21 16:54:34 -0400

    Sign In Sign Up section

--- Instagram/Managers/AuthManager.swift
+++ Instagram/Managers/AuthManager.swift
@@ -15,6 +15,11 @@ final class AuthManager {
 
     let auth = Auth.auth()
 
+    enum AuthError: Error {
+        case newUserCreation
+        case signInFailed
+    }
+
     public var isSignedIn: Bool {
         return auth.currentUser != nil
     }
@@ -24,7 +29,23 @@ final class AuthManager {
         password: String,
         completion: @escaping (Result<User, Error>) -> Void
     ) {
+        DatabaseManager.shared.findUser(with: email) { [weak self] user in
+            guard let user = user else {
+                completion(.failure(AuthError.signInFailed))
+                return
+            }
 
+            self?.auth.signIn(withEmail: email, password: password) { result, error in
+                guard result != nil, error == nil else {
+                    completion(.failure(AuthError.signInFailed))
+                    return
+                }
+
+                UserDefaults.standard.setValue(user.username, forKey: "username")
+                UserDefaults.standard.setValue(user.email, forKey: "email")
+                completion(.success(user))
+            }
+        }
     }
 
     public func signUp(
@@ -34,10 +55,45 @@ final class AuthManager {
         profilePicture: Data?,
         completion: @escaping (Result<User, Error>) -> Void
     ) {
+        let newUser = User(username: username, email: email)
+        // Create account
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                completion(.failure(AuthError.newUserCreation))
+                return
+            }
 
+            DatabaseManager.shared.createUser(newUser: newUser) { success in
+                if success {
+                    StorageManager.shared.uploadProfilePicture(
+                        username: username,
+                        data: profilePicture
+                    ) { uploadSuccess in
+                        if uploadSuccess {
+                            completion(.success(newUser))
+                        }
+                        else {
+                            completion(.failure(AuthError.newUserCreation))
+                        }
+                    }
+                }
+                else {
+                    completion(.failure(AuthError.newUserCreation))
+                }
+            }
+        }
     }
 
-    public func signOut(completion: @escaping (Bool) -> Void) {
-
+    public func signOut(
+        completion: @escaping (Bool) -> Void
+    ) {
+        do {
+            try auth.signOut()
+            completion(true)
+        }
+        catch {
+            print(error)
+            completion(false)
+        }
     }
 }
